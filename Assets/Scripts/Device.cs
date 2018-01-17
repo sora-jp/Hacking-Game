04b8fb1bee6ed33d8d7c45c530070de7efb99e4e
awerar <alexander.wahlsten@gmail.com>
2018-01-17 16:58:20 +0100

    File Changes

--- Assets/Scripts/Device.cs
+++ Assets/Scripts/Device.cs
@@ -38,6 +38,15 @@ public interface IConnectedDevice : IDevice
 public abstract class Device : MonoBehaviour, IDevice
 {
     bool status; // Basically this varible changes state whether or not the device is active
+    FileDatabase fileDatabase; // The database wich keeps controll over all the files
+
+    /// <summary>
+    /// The constructor wich initializes the filedatabase
+    /// </summary>
+    public Device(File[] files)
+    {
+        fileDatabase = new FileDatabase(files);
+    }
 
     /// <summary>
     /// Activates the device
